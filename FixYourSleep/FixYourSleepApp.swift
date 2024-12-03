11d1b5d92d9edd2012e6942b1e6cc61030d35f2a
Elif Bilge Parlak <elf.prlk64@gmail.com>
2024-12-03 18:46:37 +0100

    Initial Commit

---
+++ FixYourSleep/FixYourSleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  FixYourSleepApp.swift
+//  FixYourSleep
+//
+//  Created by Elif Parlak on 3.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct FixYourSleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
